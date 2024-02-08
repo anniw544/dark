8fa06a86e6a6191c970be2aba3cbaab95bc97eb4
William Annis <anniw544@hsd.k12.or.us>
2024-02-08 14:05:39 -0800

    pic up work

---
+++ Assets/scripts/PickupFlashlight.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupFlashlight : MonoBehaviour
+{
+    
+
+
+
+    public GameObject FlashLightOnPlayer;
+
+
+
+    void Start()
+    {
+        FlashLightOnPlayer.SetActive(false);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                this.gameObject.SetActive(false);
+                
+                FlashLightOnPlayer.SetActive(true);
+            }
+        }
+    }
+}
