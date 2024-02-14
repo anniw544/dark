a78b018e83a599ded077fb37dc6304e9cd829fae
William Annis <anniw544@hsd.k12.or.us>
2024-02-14 13:56:22 -0800

    class over

---
+++ Assets/scripts/DoorOpen.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    
+
+
+
+    public GameObject OpenedDoor;
+
+
+
+    void Start()
+    {
+        OpenedDoor.SetActive(false);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            if (Input.GetKey(KeyCode.F))
+            {
+                this.gameObject.SetActive(false);
+                
+                OpenedDoor.SetActive(true);
+            }
+        }
+    }
+}
