429c8d26d791312c85ed9e10c66f526bc8aebfcb
William Annis <anniw544@hsd.k12.or.us>
2024-03-08 14:05:24 -0800

    hell in aniamtor

--- Assets/scripts/ai/Bhand.cs
+++ Assets/scripts/ai/Bhand.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bhand : MonoBehaviour
+public class BHand : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    private Animation anim;
+
+ void Start()
     {
+         anim = gameObject.GetComponent<Animation>();
         
     }
 
-    // Update is called once per frame
-    void Update()
+
+  private void OnTriggerStay(Collider other)
     {
-        
+    if(other.gameObject.tag == "Flash")
+        {  
+            Debug.Log("Death");
+            anim.Play("kill");
+        }
+    else
+    {
+            anim.Play("BHandLmbIdle");
+    }
+    
     }
 }
