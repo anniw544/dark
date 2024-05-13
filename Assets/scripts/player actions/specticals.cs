fc90a35ddae7484f5b3de583bc338e073435457c
William Annis <anniw544@hsd.k12.or.us>
2024-05-13 13:37:27 -0700

    tut update

--- Assets/scripts/player actions/specticals.cs
+++ Assets/scripts/player actions/specticals.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class specticals : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+
+    public GameObject Light;
+    int light_on = 1;
+    public int battery = 100;
+    
+    void Start () 
     {
-        
+        light_on = 0;
+       
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Mouse1) && light_on == 0 && battery > 0)
+        {
+            Light.SetActive(true);
+            StartCoroutine(LightOn());
+             
+        }
+
+        if (Input.GetKey(KeyCode.Mouse1) && light_on == 1)
+        {
+            Light.SetActive(false);
+            StartCoroutine(LightOff());
+             
+        }
+// shabingus
+        if (battery > 0 && light_on == 1)
+        {
+            battery -= 1;
+        }
+
+        if (battery == 0)
+        {
+            Light.SetActive(false);
+            light_on = 0;
+        }
+    }
+
+        private IEnumerator LightOn()
+    { 
+            yield return new WaitForSeconds(1);
+            light_on = 1;    
+    }
+
+    private IEnumerator LightOff()
+    { 
+            yield return new WaitForSeconds(1);
+            light_on = 0;
     }
 }
